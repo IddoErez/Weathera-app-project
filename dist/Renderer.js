e075fb0e9e095de51cc48b710973ed469d4780d7
Ido Erez <idodoerez@gmail.com>
2020-12-03 02:24:11 +0200

    Finished api routes, cityManager class

---
+++ dist/Renderer.js
@@ -0,0 +1,20 @@
+// class Renderer {
+//     constructor() {
+//          }
+
+//     renderExpenses(expensesList) {
+//         $("#expenses-list").empty()
+//         $("#newExpense").empty()
+//         const source = $("#Expenses-template").html()
+//         const template = Handlebars.compile(source)
+//         let newHtml = template({ expensesList })
+//         $("#expenses-list").append(newHtml)
+//     }
+//     renderAdd(NewExpense) {
+//         $("#newExpense").empty()
+//         const source = $("#addExpense-template").html()
+//         const template = Handlebars.compile(source)
+//         let newHtml = template( NewExpense )
+//         $("#newExpense").append(newHtml)
+//     }
+// }
---
+++ dist/main.js
@@ -0,0 +1,19 @@
+// let expense = new ExpenseManager()
+// let renderer = new Renderer()
+
+// $("#addExpense").on('click', async function () {
+//     $("#expenses-list").empty()
+//     const city = $('#city').val()
+// }
+//    let x = await expense.addExpense(city)
+//     console.log(item)
+//     console.log(data)
+//     renderer.renderAdd(data)
+//     return x
+//     })
+
+// $("#showExpenses").on('click', async function () {
+//     let data = await expense.getExpenses()
+//     console.log(data)
+//     renderer.renderExpenses(data)
+// })
