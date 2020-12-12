8d488803f5c446ec21c291b7318ac1c42d988322
Ido Erez <idodoerez@gmail.com>
2020-12-12 21:07:24 +0200

    changed DB line

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose')
 const path = require('path')
 require('dotenv').config()
 const {PORT} = process.env
-mongoose.connect(process.env.MONGODB_URI||'mongodb://localhost/yourDB'
+mongoose.connect(process.env.MONGODB_URI||'mongodb://localhost/CitiesDB'
 , { useNewUrlParser: true })
 app.use(express.static(path.join(__dirname, 'dist')))
 app.use(express.static(path.join(__dirname, 'node_modules')))
