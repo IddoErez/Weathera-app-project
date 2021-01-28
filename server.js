79f767308f854af7f60392d89b72b8c0bc729d97
Ido Erez <idodoerez@gmail.com>
2021-01-28 18:03:03 +0200

    ready for deployment

--- server.js
+++ server.js
@@ -5,9 +5,8 @@ const mongoose = require('mongoose')
 const path = require('path')
 require('dotenv').config()
 
-// mongoose.connect(process.env.MONGODB_URI||'mongodb://localhost/CitiesDB')
-mongoose.connect("mongodb+srv://idodoerez:Tradingpostfinalproject135@cluster0.d2ipx.mongodb.net/citiesDB?retryWrites=true&w=majority")
-// , { useNewUrlParser: true })
+mongoose.connect(process.env.MONGODB_URI||'mongodb://localhost/CitiesDB')
+
 app.use(express.static(path.join(__dirname, 'dist')))
 app.use(express.static(path.join(__dirname, 'node_modules')))
 
